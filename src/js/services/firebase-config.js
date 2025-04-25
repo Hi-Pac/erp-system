8a037bc0cde7c40c8563b90ece4d3530062776c6
Hi-Pac <mo.tiger.789@gmail.com>
2025-04-25 16:39:09 +0300

    Update firebase-config.js

--- src/js/services/firebase-config.js
+++ src/js/services/firebase-config.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase
 
 // HCP ERP Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBDjszR5bbHZeJDUowuer3S-Stx6cKHLOc",
-    authDomain: "hcp-erp-1b930.firebaseapp.com",
-    projectId: "hcp-erp-1b930",
-    storageBucket: "hcp-erp-1b930.firebasestorage.app",
-    messagingSenderId: "1037329690081",
-    appId: "1:1037329690081:web:6659b5f072cb63b43436a0",
-    measurementId: "G-DMT14M43MY"
+  apiKey: "AIzaSyD7O1-SFhY3XUc4ZL2MWo83nl-DfvGq3LI",
+  authDomain: "erp-system-24f05.firebaseapp.com",
+  projectId: "erp-system-24f05",
+  storageBucket: "erp-system-24f05.firebasestorage.app",
+  messagingSenderId: "1015081257227",
+  appId: "1:1015081257227:web:ab95b6dbe9b678ef4cba0e",
+  measurementId: "G-3VH3396ZDG"
 };
 
 // Initialize Firebase services
